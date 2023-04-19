e919b0bea1ef0af92e2029491ed4336bf3c8914e
Lawrence <68629118+LawrenceChanKokWai@users.noreply.github.com>
2023-04-19 21:52:10 +0800

    initial commit

---
+++ wp-config.php
@@ -0,0 +1,8 @@
+define('AUTH_KEY',         '?^H~|&|ho[lQT~pK.F2_T%1g.n,]WnW45L:3>el]wM8P$)XEc7hJ^PHw6G^.O3&I');
+define('SECURE_AUTH_KEY',  'lVd%mM+nyep0TDFLY3x|yAS+z7#^avtKHsNE<K;-6BjHK-mQS=(voP.G-2X}9<*=');
+define('LOGGED_IN_KEY',    'YOqn`^m)y29TAT?XRBB|mF!|bVHy{3efhO_(XTKNm=GwOfr=a-1i,uW[mfZ[D|][');
+define('NONCE_KEY',        'N*,Z33fuRO[bC y!:g,jDk]YI+cZ57U*:<A(vk.6z+S1V:hjvvmdK=6`hmIrj6cp');
+define('AUTH_SALT',        'j2}b XW rAO$Sp^}6,#-oWyc^{j7rna~J6,O/Ek&gcOD2hOdt;>Gq707>Ie|fSE!');
+define('SECURE_AUTH_SALT', 'mL6*4ylY,DC_e+uhYs#|A%^0fMiG;e!e:{Ujp3wXni:bJyDUR.~G-rt#h4I}p>{:');
+define('LOGGED_IN_SALT',   '%|]_A^2HTU5s_:Bx}6*oN_TpY|yAp+nt}7MV%U.qh<*A#^4D_4]/E5H#^Sa&4de=');
+define('NONCE_SALT',       'G8=e0X&;3G[um?u>]UqwbOXw-K~|d,Y;G:8``LUTfzjW3}ZDxtYL0BGoGvJacY05');
